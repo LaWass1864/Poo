492b3b7c31193de1ce5650a9e962fe8dab9ad1f2
lawass1864 <amwass4@hotmail.com>
2024-01-01 22:38:02 +0100

    methode js a 25:12

--- cours/index.js
+++ cours/index.js
@@ -3,7 +3,11 @@ const obj = {
   //index:value
   pseudo: "Wass",
   ville: "St-Denis",
-  admin: true
+  admin: true,
+
+  direBonjour: function() {
+    console.log("Bonjour je suis " + this.pseudo);
+  }
 };
 
 // Ajouter une valeur dans .obj
@@ -13,7 +17,7 @@ const obj = {
 obj["admin"] = false
 
 // Modifier un parametre
-obj.pseudo = "Wassou"
+obj.pseudo = "wass"
 
 // Supprimer un parametre
 delete obj.ville;
@@ -25,8 +29,35 @@ delete obj.ville;
 
 // Parcourir l'objet
 for (const key in obj) {
-    console.log(key + " : " + obj[key]);
+    // console.log(key + " : " + obj[key]);
+}
+// console.log(obj.direBonjour());
+
+// Méthodes natives dans JavaScript
+
+const keys = Object.keys(obj)
+console.log(keys);
+
+// Obtenir les valeurs
+const values = Object.values(obj)
+console.log(values);
+
+const nestedArray = Object.entries(obj)
+console.log(nestedArray);
+
+// Fusionner des objets entre eux
+
+const obj2 = {
+    taille: "1m78",
+    poids: "60kg",
+    pseudo: "wassou"
 }
 
+const fusion = Object.assign({}, obj, obj2);
+console.log(fusion);
 
-
+// Empecher les modifications
+const newObj = Object.freeze(obj);
+newObj.pseudo = "Wassila"
+newObj.adress = "42 avenue du code"
+console.log(newObj);
